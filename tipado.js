1edbcfda778c28a99cd8fc7951674a716b38e46e
moonhunter <vdcloayza@gmail.com>
2021-12-01 23:32:30 -0400

    subiendo practicas

---
+++ tipado.js
@@ -0,0 +1,33 @@
+// var numero=5//tipo int
+// let nombre='pablo'//tipo string
+// const edad=37 //tipo entero
+// //variable
+// let name2:string='diamante'
+// console.log(name2)
+// name2: 'dueño'
+// //no olvidar let y const
+// let play:boolean//declaro aquí que será booleano con let
+// play:'true'//nombre y asignacion de su valor de la varialbe
+// let digito:number=10
+// console.log(digito)
+// let numero:number[]=[1,2,3]
+// let names:string[]=['lagarto-chan','inocencio','santuso']
+// let coleccionnumber:Array<number>=[1,2,3]
+// // let coleccionnnames:Array<string>=['lagarto-chan','inocencio','santuso']
+// let platu:[boolean,number,string]
+// platu=[false,23,'michiSan']
+// console.log(platu[2])
+// function sum(z:number,x:number):number{
+//     return z+x
+// }
+// const suma:number=
+// sum(1.5,1.5);
+// console.log(suma);
+function show() {
+    console.log('Function 1');
+}
+var show2 = function () {
+    console.log('function2');
+};
+show();
+show2();
---
+++ tipado.ts
@@ -0,0 +1,35 @@
+// var numero=5//tipo int
+// let nombre='pablo'//tipo string
+// const edad=37 //tipo entero
+// //variable
+// let name2:string='diamante'
+// console.log(name2)
+// name2: 'dueño'
+// //no olvidar let y const
+// let play:boolean//declaro aquí que será booleano con let
+// play:'true'//nombre y asignacion de su valor de la varialbe
+
+// let digito:number=10
+// console.log(digito)
+// let numero:number[]=[1,2,3]
+// let names:string[]=['lagarto-chan','inocencio','santuso']
+// let coleccionnumber:Array<number>=[1,2,3]
+// // let coleccionnnames:Array<string>=['lagarto-chan','inocencio','santuso']
+// let platu:[boolean,number,string]
+// platu=[false,23,'michiSan']
+// console.log(platu[2])
+// function sum(z:number,x:number):number{
+//     return z+x
+// }
+// const suma:number=
+// sum(1.5,1.5);
+// console.log(suma);
+//this is a arrow functions
+function show():void{
+    console.log('Function 1')
+}
+const show2=():void=>{
+    console.log('function2')
+}
+show();
+show2();
